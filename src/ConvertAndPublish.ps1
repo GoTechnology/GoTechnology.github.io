0447cc41a97c5eb889e03cd2e5d40619ec29f1c6
JoshGoolnik <joshgoolnik@hotmail.com>
2017-08-22 10:32:07 +0100

    Testing

--- src/ConvertAndPublish.ps1
+++ src/ConvertAndPublish.ps1
@@ -3,7 +3,7 @@ Write-Host "Pulling latest branch"
 &git pull
 cd src
 $commitMessage = Read-Host "Please enter your commit message (or press CTRL+C to cancel)"
-& Convert
+& Convert.ps1
 cd..
 & git add .
 & git commit -m $commitMessage

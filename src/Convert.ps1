020dfccc3d54d323ea3c2d051c95450f9949774b
JoshGoolnik <joshgoolnik@hotmail.com>
2017-08-22 14:40:33 +0100

    Formatting fixed for the most part

--- src/Convert.ps1
+++ src/Convert.ps1
@@ -1,12 +1,12 @@
 # Get the date and time
 $updatedOn = Get-Date
-# header info
-$head = "<title>hub2 Roadmap</title>
+# header + footer info
+$header = "<title>hub2 Roadmap</title>
 <link rel=`"stylesheet`" href=`"site.css`"/>
 <link href=`"https://fonts.googleapis.com/css?family=PT+Sans`" rel=`"stylesheet`">"
 
 # title, will be injected before the filter file and shown at the top.
-$title = "<h1>hub2 Roadmap</h1>
+$title = "<h1>hub2<sup>&trade;</sup> Roadmap</h1>
 <div>
     <span class=`"red-text left`"><span class=`"bold`">WARNING:</span> This is a living document and subject to continual change.</span>
     <span class=`"right italic`">Last updated " + $updatedOn.ToLongDateString() + " " + $updatedOn.ToLongTimeString() + "</span>
@@ -84,9 +84,10 @@ cd GoTechnology\qedi-GoTechnology.github.io\src
 # import the CSV and pipe through to the Convert command, add the header then (before the contents of Filter.csv) 
 # add the title. After the contents of Filter.csv add the key tables and output using utf8 encoding (so the css 
 # works correctly
-import-csv Filter.csv | ConvertTo-Html -head $head -PreContent $title -PostContent $key| Out-File -Encoding utf8 ..\index.html
+import-csv Filter.csv | ConvertTo-Html -head $header -PreContent $title -PostContent $key | Out-File -Encoding utf8 ..\index.html
 
 # Style the Condition fields by performing three find and replace operations.
+(Get-Content ..\index.html).Replace('<table>','<table class = "table-work table-striped">') | Set-Content ..\index.html
 (Get-Content ..\index.html).Replace('<td>Green</td>','<td class = "green">Green</td>') | Set-Content ..\index.html
 (Get-Content ..\index.html).Replace('<td>Yellow</td>','<td class = "yellow">Yellow</td>') | Set-Content ..\index.html
 (Get-Content ..\index.html).Replace('<td>Red</td>','<td class = "red">Red</td>') | Set-Content ..\index.html

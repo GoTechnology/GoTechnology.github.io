d71b4bb0664a6d0a8b2dda9135e84689a879ee9f
JoshGoolnik <joshgoolnik@hotmail.com>
2017-08-22 10:27:19 +0100

    Lots of changes, new powershell script

---
+++ src/ConvertAndPublish.ps1
@@ -0,0 +1,12 @@
+cd C:\GoTechnology\qedi-GoTechnology.github.io
+Write-Host "Pulling latest branch"
+&git pull
+cd src
+$commitMessage = Read-Host "Please enter your commit message (or press CTRL+C to cancel)"
+& Convert
+cd..
+& git add .
+& git commit -m $commitMessage
+& git push
+#Pause after running in case there were any errors
+Read-Host "Press any key to exit."

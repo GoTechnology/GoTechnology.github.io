f04810db90890676fa082c439642cbdda283b7ce
JoshGoolnik <joshgoolnik@hotmail.com>
2017-12-07 14:59:46 +0000

    Restructuring site, adding V1.1 patchnotes and updating known issues.

--- src/Convert.ps1
+++ src/Convert.ps1
@@ -1,25 +1,15 @@
 # Get the date and time
 $updatedOn = Get-Date
 # header + footer info
-$header_Roadmap = "<title>hub2 Roadmap</title>
-<link rel=`"stylesheet`" href=`"site.css`"/>
+$header = "<title>hub2 Known Issues</title>
+<link rel=`"stylesheet`" href=`"..\site.css`"/>
 <link href=`"https://fonts.googleapis.com/css?family=PT+Sans`" rel=`"stylesheet`">
-<link rel=`"shortcut icon`" href=`"favicon.ico`">"
-$header_Issues = "<title>hub2 Known Issues</title>
-<link rel=`"stylesheet`" href=`"site.css`"/>
-<link href=`"https://fonts.googleapis.com/css?family=PT+Sans`" rel=`"stylesheet`">
-<link rel=`"shortcut icon`" href=`"favicon.ico`">"
+<link rel=`"shortcut icon`" href=`"..\favicon.ico`">"
 
 # title, will be injected before the filter file and shown at the top.
-$title_Roadmap = "<h1><img src=`"qedilogodevice_white.png`"> hub2 Roadmap</h1>
+$title = "<h1><img src=`"..\qedilogodevice_white.png`"> hub2 Known Issues</h1>
 <div>
-    <span class=`"red-text left`"><span class=`"bold`">WARNING:</span> This is a living document and subject to continual change. <a href=`"issues.html`">Known Issues List</a></span>
-    <span class=`"right italic`">Last updated " + $updatedOn.ToLongDateString() + " " + $updatedOn.ToLongTimeString() + "</span>
-    <br/>
-</div>"
-$title_Issues = "<h1><img src=`"qedilogodevice_white.png`"> hub2 Known Issues</h1>
-<div>
-    <span class=`"red-text left`"><span class=`"bold`">WARNING:</span> This is a living document and subject to continual change. <a href=`"index.html`">Feature Roadmap</a></span>
+    <span class=`"red-text left`"><span class=`"bold`">WARNING:</span> This is a living document and subject to continual change.</span>
     <span class=`"right italic`">Last updated " + $updatedOn.ToLongDateString() + " " + $updatedOn.ToLongTimeString() + "</span>
     <br/>
 </div>"
@@ -45,67 +35,20 @@ $key = "<table class=`"table-key table-striped`">
 				<td>External or severe blockers.</td>
 			<tr>
 		</tbody>
-	</table>
-	<div class=`"table-key`"><hr/></div>
-	<table class=`"table-key table-striped`">
-		<thead>
-			<tr class=`"head`">
-				<th>Class</th>
-				<th>Description</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>A</td>
-				<td>Extremely high.</td>
-			</tr>
-			<tr>
-				<td>B</td>
-				<td>Very high.</td>
-			</tr>
-			<tr>
-				<td>C</td>
-				<td>Quite high.</td>
-			</tr>
-			<tr>
-				<td>D</td>
-				<td>Moderate</td>
-			</tr>
-			<tr>
-				<td>E</td>
-				<td>Quite low.</td>
-			</tr>
-			<tr>
-				<td>F</td>
-				<td>Very low.</td>
-			</tr>
-			<tr>
-				<td>G</td>
-				<td>Extremely low.</td>
-			</tr>
-		</tbody>
-	</table>
-<div class=`"table-key`">Request Index Class represents an amalgamated ranking of possible modifications based on business and client drivers.<hr/></div>
-	"
+	</table>"
 
 # Make sure we're in the right place
+c:
 cd\
 cd GoTechnology\qedi-GoTechnology.github.io\src
 
 # import the CSV and pipe through to the Convert command, add the header then (before the contents of Filter.csv) 
 # add the title. After the contents of Filter.csv add the key tables and output using utf8 encoding (so the css 
 # works correctly
-import-csv Filter.csv | ConvertTo-Html -head $header_Roadmap -PreContent $title_Roadmap -PostContent $key | Out-File -Encoding utf8 ..\index.html
+import-csv Filter.csv | ConvertTo-Html -head $header -PreContent $title -PostContent $key | Out-File -Encoding utf8 ..\i\index.html
 
 # Style the Condition fields by performing three find and replace operations.
-(Get-Content ..\index.html).Replace('<table>','<table class = "table-work table-striped">') | Set-Content ..\index.html
-(Get-Content ..\index.html).Replace('<td>Green</td>','<td class = "green">Green</td>') | Set-Content ..\index.html
-(Get-Content ..\index.html).Replace('<td>Yellow</td>','<td class = "yellow">Yellow</td>') | Set-Content ..\index.html
-(Get-Content ..\index.html).Replace('<td>Red</td>','<td class = "red">Red</td>') | Set-Content ..\index.html
-
-import-csv Issues.csv | ConvertTo-Html -head $header_Issues -PreContent $title_Issues -PostContent $key | Out-File -Encoding utf8 ..\issues.html
-# Style the Condition fields by performing three find and replace operations.
-(Get-Content ..\issues.html).Replace('<table>','<table class = "table-work table-striped">') | Set-Content ..\issues.html
-(Get-Content ..\issues.html).Replace('<td>Green</td>','<td class = "green">Green</td>') | Set-Content ..\issues.html
-(Get-Content ..\issues.html).Replace('<td>Yellow</td>','<td class = "yellow">Yellow</td>') | Set-Content ..\issues.html
-(Get-Content ..\issues.html).Replace('<td>Red</td>','<td class = "red">Red</td>') | Set-Content ..\issues.html
+(Get-Content ..\i\index.html).Replace('<table>','<table class = "table-work table-striped">') | Set-Content ..\i\index.html
+(Get-Content ..\i\index.html).Replace('<td>Green</td>','<td class = "green">Green</td>') | Set-Content ..\i\index.html
+(Get-Content ..\i\index.html).Replace('<td>Yellow</td>','<td class = "yellow">Yellow</td>') | Set-Content ..\i\index.html
+(Get-Content ..\i\index.html).Replace('<td>Red</td>','<td class = "red">Red</td>') | Set-Content ..\i\index.html

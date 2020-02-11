92b5b84878f6053d60e6550c304e140780c6a96f
JoshGoolnik <joshgoolnik@hotmail.com>
2020-02-11 11:56:39 +0000

    Updating script and adding 1.27 release notes

--- src/Convert.ps1
+++ src/Convert.ps1
@@ -3,15 +3,56 @@ c:
 cd\
 cd GoTechnology\GoTechnology.github.io\src
 
-# Get the date and time
-$updatedOn = Get-Date
-# header + footer info
+# header
 $header = Get-Content -Path header.txt
 
-# top, will be injected before the filter file and shown at the top.
+# top, will be injected before the version list and shown at the top.
 [string]$top = Get-Content -Path top.txt
 
-# import the CSV and pipe through to the Convert command, add the header then (before the contents of Filter.csv) 
-# add the title. After the contents of Filter.csv add the key tables and output using utf8 encoding (so the css 
-# works correctly
-import-csv Filter.csv | ConvertTo-Html -head $header -PreContent $top | Out-File -Encoding utf8 ..\index.html
+#bottom, will be injected after the version list and before the known issues
+[string]$bottom = Get-Content -Path bottom.txt
+
+c:
+cd\
+cd .\gotechnology\gotechnology.github.io\v
+
+$versionOutputArrayList = [System.Collections.ArrayList]@()
+$versionInputArray = Get-ChildItem -Directory | Sort-Object name -Descending
+foreach ($version in $versionInputArray)
+{
+    $Row = "" | Select Version, Docx, PDF
+    cd $version #go to the version directory so we can see if it's empty or not
+
+
+    # it will be empty if it's a maintenance release, so we remove the links and add asterisks
+    if( (Get-ChildItem | Measure-Object).Count -eq 0)
+    {
+        $Row.Version = $ExecutionContext.InvokeCommand.ExpandString('$version')
+        $Row.Docx = $ExecutionContext.InvokeCommand.ExpandString('*')
+        $Row.PDF = $ExecutionContext.InvokeCommand.ExpandString('*')
+    }
+    # otherwise we add the links to the documents
+    else
+    {
+        $Row.Version = $ExecutionContext.InvokeCommand.ExpandString('<a href=v/$version/$version.htm>$version</a>')
+        $Row.Docx = $ExecutionContext.InvokeCommand.ExpandString('<a href=v/$version/$version.docx>.docx</a>')
+        $Row.PDF = $ExecutionContext.InvokeCommand.ExpandString('<a href=v/$version/$version.pdf>.pdf</a>')
+    }
+    $versionOutputArrayList.Add($Row)
+    cd ..
+}
+#convert the version list to HTML - when we do this it will change the < and > to &lt; etc, so we need to rewrite those
+$htmlOutput = $versionOutputArrayList | ConvertTo-Html -head $header -PreContent $top -PostContent $bottom | foreach {$_.replace("&lt;","<").replace("&gt;",">")}
+
+#navigate to the folder where the known issues are stored
+c:
+cd\
+cd GoTechnology\GoTechnology.github.io\src
+#add in the known issues table
+$htmlOutput += import-csv Filter.csv | ConvertTo-Html -Fragment 
+
+#add in the closing tags
+$htmlOutput += "</body></html>" 
+
+#fix the table styling and then write the file to index.html
+$htmlOutput | foreach {$_.replace('<table>','<table class="table table-sm table-bordered table-striped"')} | Out-File -Encoding utf8 ..\index.html
